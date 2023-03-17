d23685a0205ebf86bc5373edd2bce5c9b997f326
umutpamuk <umutpamuk59@gmail.com>
2023-03-17 17:38:01 +0300

    Tablolar arası ilişki oluşturuldu.

--- app/Models/Order_item.php
+++ app/Models/Order_item.php
@@ -9,4 +9,12 @@ class Order_item extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public function order(){
+        return $this->belongsTo(Order::class,'order_id','id');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,12 @@ class Product extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function author() {
+        return $this->belongsTo(Author::class,'author_id', 'id');
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
