6f02b6bc4604bf7dbdfa292e4dfe463eb28ee357
umutpamuk <umutpamuk59@gmail.com>
2023-03-18 14:58:28 +0300

    Kampanaya tablosu için seedler eklendi.

--- database/seeders/ProductJsonSeeder.php
+++ database/seeders/ProductJsonSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Author;
+use App\Models\Campaign;
+use App\Models\CampaignDetail;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
@@ -22,15 +24,52 @@ class ProductJsonSeeder extends Seeder
      */
     public function run()
     {
-        $productJsonFile = File::get('database/data/products.json');
-        $products = json_decode($productJsonFile);
-
         User::insert([
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'password' => Hash::make(fake()->password(6,10)) // password
         ]);
 
+        $fakeCampaignArray = [
+            [
+                'campaign_name'     => "Sabahattin Ali'nin Roman kitaplarında 2 üründen 1 tanesi bedava.",
+                'campaign_discount' => '100',
+                'condition_author'  => 3
+            ], [
+                'campaign_name'     => "Yerli Yazar Kitaplarında %5 indirim",
+                'campaign_discount' => '0.95',
+                'condition_author_nationality'  => 3
+            ], [
+                'campaign_name'     => "200 TL ve üzeri alışverişlerde sipariş toplamına %5 indirim",
+                'campaign_discount' => '0.95',
+                'condition_amount'  => 200
+            ]
+
+        ];
+        foreach ($fakeCampaignArray as $fakeCampaign) {
+
+
+            $campaignId = Campaign::insertGetId([
+                'campaign_name'     => $fakeCampaign['campaign_name'],
+                'campaign_discount' => $fakeCampaign['campaign_discount'],
+                'created_at'        => Carbon::now()
+            ]);
+            if ($campaignId) {
+
+                $fakeValue = end($fakeCampaign);
+                $conditionIndex = key($fakeCampaign);
+                CampaignDetail::insert([
+                    'campaign_id'   => $campaignId,
+                    $conditionIndex => $fakeValue,
+                    'created_at'    => Carbon::now()
+
+                ]);
+            }
+
+        }
+
+        $productJsonFile = File::get('database/data/products.json');
+        $products = json_decode($productJsonFile);
         foreach ($products as $item) {
 
             $author = Author::firstOrCreate([
