9b37e816002aff472ec5bdc2bdeda61d51553e6f
umutpamuk <umutpamuk59@gmail.com>
2023-03-18 15:07:07 +0300

    Sipariş oluşturma servisi için route eklendi. Requestten gelen veri kontrolleri için CreateOrderRequest.php eklendi.

---
+++ app/Http/Requests/CreateOrderRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'productItems' => 'required|array|min:1',
+            'productItems.*.product_id' =>  [
+                'required',
+                Rule::exists('products', 'id')
+            ],
+            'productItems.*.product_quantity' => 'required|integer|min:1',
+            'checkoutDetails' => 'required|array',
+            'checkoutDetails.address' => 'required|string',
+        ];
+    }
+}
