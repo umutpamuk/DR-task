211717c39a44e8b90d3c0a20b2384c147367e01b
umutpamuk <umutpamuk59@gmail.com>
2023-03-19 14:03:46 +0300

    Sipariş detaları servisi oluşturuldu.

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::controller(OrderController::class)->group(function(){
     Route::post('/create-order' , 'createOrder');
+    Route::get('/order-details/{orderNumber}' , 'orderDetails');
 });
