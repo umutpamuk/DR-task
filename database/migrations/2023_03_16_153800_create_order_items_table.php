56a56eb55849b599cd23559ed8144c561d9b2645
umutpamuk <umutpamuk59@gmail.com>
2023-03-19 13:56:19 +0300

    campaign_id sütununa nullable özelliği eklendi.

--- database/migrations/2023_03_16_153800_create_order_items_table.php
+++ database/migrations/2023_03_16_153800_create_order_items_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->integer('quantity');
             $table->double('selling_price');
             $table->double('discount_price');
-            $table->unsignedBigInteger('campaign_id');
+            $table->unsignedBigInteger('campaign_id')->nullable();
             $table->timestamps();
         });
     }
