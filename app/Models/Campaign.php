afe4c2bde95bb478a8f61612855ae5f5316d8fc2
umutpamuk <umutpamuk59@gmail.com>
2023-04-10 01:10:32 +0300

    Return type eklendi.

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -4,19 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Campaign extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function campaignDetail() {
+    public function campaignDetail() : BelongsTo
+    {
         return $this->belongsTo(CampaignDetail::class, 'id', 'campaign_id');
-    }
-
-    public static function activeCampaigns() {
-
-        return self::where('status',1)->get();
-
     }
 }
